ccd89cc1b85e80912d15506d55803d36ff13eaf3
Gulya1098 <mansurova221098@gmail.com>
2024-12-13 17:48:23 +0500

    Add files via upload

---
+++ 'module_3.py'.py
@@ -0,0 +1,13 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Jonny', 'Bilbo', 'Steve', 'Kendrik', 'Aaron'}
+students_sorted = sorted(students)
+print(students)
+
+grades_average_list = [(sum(grades[0])) / (len(grades[0])), (sum(grades[1])) / (len(grades[1])), (sum(grades[2])) /
+(len(grades[2])), (sum(grades[3])) / (len(grades[3])), (sum(grades[4])) / (len(grades[4]))]
+print(grades_average_list)
+
+grades_average_dict = dict(zip(students_sorted, grades_average_list))
+print(grades_average_dict)
+
+
